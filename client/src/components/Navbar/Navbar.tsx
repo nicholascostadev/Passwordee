4b783a6477c70bf110ba0e7a9b1503b14e951d0f
Nicholas <nicholascostadev@gmail.com>
2022-03-04 21:15:25 -0300

    Add responsiveness to sidebar with animation

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import { User } from '../../Interfaces';
 import { useNavigate } from 'react-router-dom';
 import MenuIcon from '@mui/icons-material/Menu';
+import ClearIcon from '@mui/icons-material/Clear';
 
 function Navbar(): JSX.Element {
 	const [showMenu, setShowMenu] = useState<boolean>(false);
@@ -43,18 +44,29 @@ function Navbar(): JSX.Element {
 	}, []);
 
 	const mobileSidebar = document.querySelector('.mobileSidebar');
+	const closeIcon = document.querySelector('.closeIcon');
+	const menuIcon = document.querySelector('.menuIcon');
 
 	useEffect(() => {
 		if (showMenu) {
 			mobileSidebar?.classList.remove('hide');
 			mobileSidebar?.classList.remove('hidden');
+			menuIcon?.classList.add('hidden');
+			closeIcon?.classList.remove('hidden');
 		} else {
 			mobileSidebar?.classList.add('hide');
+			closeIcon?.classList.add('hidden');
+			menuIcon?.classList.remove('hidden');
 			setTimeout(() => {
 				mobileSidebar?.classList.add('hidden');
 			}, 500);
 		}
-	}, [showMenu, mobileSidebar?.classList]);
+	}, [
+		showMenu,
+		mobileSidebar?.classList,
+		closeIcon?.classList,
+		menuIcon?.classList,
+	]);
 
 	return (
 		<div className="navbar">
@@ -78,7 +90,8 @@ function Navbar(): JSX.Element {
 							className="hamburger"
 							onClick={() => setShowMenu(!showMenu)}
 						>
-							<MenuIcon sx={{ fontSize: 'xlarge' }} />
+							<MenuIcon className="menuIcon" sx={{ fontSize: 'xlarge' }} />
+							<ClearIcon className="closeIcon" />
 						</button>
 						<div className="mobileSidebar hide hidden">
 							<p>{user.email}</p>
